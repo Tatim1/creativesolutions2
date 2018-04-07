6ce068ef6e38627be05807eb283c0ec7b00b815e
Tatim1 <abrabd98@gmail.com>
2018-04-07 18:08:10 +0300

    first

---
+++ src/js/scripts.js
@@ -0,0 +1,27 @@
+// $(document).ready(function() {
+
+	
+	
+	
+
+// $('body').on('click', '.scrollTo', function(e){
+// 					e.preventDefault();
+// 					var target = $(this).attr('href'),
+// 						offset = Math.floor($(target).offset().top);
+// 					$('body').toggleClass('nav-opened');
+// 					$('html').animate({
+// 						scrollTop: offset
+// 					}, 500, function()
+// 					{
+// 						if (offset <= 0) {
+// 							$('header').removeClass('fixed-top');
+// 						} else {
+// 							$('header').addClass('fixed-top');							
+// 						}
+// 					});
+// 				})
+
+	
+	
+
+// 	});
