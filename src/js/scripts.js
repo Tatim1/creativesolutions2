867ff7b75e490fcd4dc42b9a3ff9bb85611f4900
Tatim1 <abrabd98@gmail.com>
2018-04-10 10:00:14 +0300

    111

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,27 +1,36 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
 	
 	
 	
 
-// $('body').on('click', '.scrollTo', function(e){
-// 					e.preventDefault();
-// 					var target = $(this).attr('href'),
-// 						offset = Math.floor($(target).offset().top);
-// 					$('body').toggleClass('nav-opened');
-// 					$('html').animate({
-// 						scrollTop: offset
-// 					}, 500, function()
-// 					{
-// 						if (offset <= 0) {
-// 							$('header').removeClass('fixed-top');
-// 						} else {
-// 							$('header').addClass('fixed-top');							
-// 						}
-// 					});
-// 				})
+$('body').on('click', '.scrollTo', function(e) {
+		e.preventDefault();
+		var target = $(this).attr('href');
+		var offset = Math.floor($(target).offset().top);
+		$('html').animate({
+			scrollTop: offset
+		}, 800, function() {
 
-	
-	
+		});
+})
 
-// 	});
+	// $('#inputEmail3, #inputPassword3, #message').on('input', function(){
+	// 	if ($('#inputEmail3').val().trim().length > 2 &&
+	// 		$('#inputPassword3').val().trim().length > 2 &&
+	// 		$('#message').val().trim().length > 2) {
+	// 			$('#send').removeAttr('disabled');
+	// 	} else {
+	// 		$('#send').attr('disabled', 'disabled');
+	// 	};
+	// });
+
+	// $('#send').on('click', function(e){
+	// 	e.preventDefault();
+	// 	$('#myModal').find('.modal-title').text('Success');
+	// 	$('#myModal').find('.modal-body p').text('You have successfully send your message.');
+	// 	$('#myModal').modal();
+	// });
+	
+});
+
