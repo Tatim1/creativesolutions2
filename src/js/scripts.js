74b93045013db111b6468669535c9a41d6a329a1
Tatim1 <32951318+Tatim1@users.noreply.github.com>
2018-04-10 10:08:13 +0300

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,22 +15,7 @@ $('body').on('click', '.scrollTo', function(e) {
 		});
 })
 
-	// $('#inputEmail3, #inputPassword3, #message').on('input', function(){
-	// 	if ($('#inputEmail3').val().trim().length > 2 &&
-	// 		$('#inputPassword3').val().trim().length > 2 &&
-	// 		$('#message').val().trim().length > 2) {
-	// 			$('#send').removeAttr('disabled');
-	// 	} else {
-	// 		$('#send').attr('disabled', 'disabled');
-	// 	};
-	// });
-
-	// $('#send').on('click', function(e){
-	// 	e.preventDefault();
-	// 	$('#myModal').find('.modal-title').text('Success');
-	// 	$('#myModal').find('.modal-body p').text('You have successfully send your message.');
-	// 	$('#myModal').modal();
-	// });
+	
 	
 });
 
